6df65c166b2fe58b2b8dec6bb12eb82fe5f5e854
prashath Sivayanama <prashsiva@hotmail.com>
2026-06-17 16:13:27 +0200

    fix

--- src/scout.rs
+++ src/scout.rs
@@ -1,6 +1,7 @@
 use rand::prelude::IndexedRandom;
 use std::collections::HashSet;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration;
@@ -14,10 +15,16 @@ pub struct Scout {
     pub known_resources: HashSet<Pos>,
     pub tx: Sender<RobotMessage>,
     pub map: Arc<Map>,
+    pub shutdown: Arc<AtomicBool>,
 }
 
 impl Scout {
-    pub fn new(id: usize, tx: Sender<RobotMessage>, map: Arc<Map>) -> Self {
+    pub fn new(
+        id: usize,
+        tx: Sender<RobotMessage>,
+        map: Arc<Map>,
+        shutdown: Arc<AtomicBool>,
+    ) -> Self {
         let base_pos = map.base_pos;
         Self {
             id,
@@ -26,13 +33,14 @@ impl Scout {
             known_resources: HashSet::new(),
             tx,
             map,
+            shutdown,
         }
     }
 
     pub fn run(mut self) {
         let mut rng = rand::rng();
 
-        loop {
+        while !self.shutdown.load(Ordering::Relaxed) {
             self.scan_surroundings();
 
             if let Some(next_pos) = self.choose_next_move(&mut rng) {
@@ -43,64 +51,66 @@ impl Scout {
         }
     }
 
-    pub fn scan_surroundings(&mut self) {
-        let directions = [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)];
+    fn scan_surroundings(&mut self) {
+        self.check_tile(self.pos);
 
-        for (dx, dy) in directions {
-            let nx = self.pos.x as i32 + dx;
-            let ny = self.pos.y as i32 + dy;
+        for target_pos in self.valid_adjacent_positions(self.pos) {
+            self.check_tile(target_pos);
+        }
+    }
 
-            if nx >= 0 && ny >= 0 {
-                let target_pos = Pos {
-                    x: nx as usize,
-                    y: ny as usize,
-                };
-
-                if self.map.in_bounds(target_pos) {
-                    if let Some(tile) = self.map.get_tile(target_pos) {
-                        match tile {
-                            Tile::Obstacle => {
-                                if self.known_obstacles.insert(target_pos) {
-                                    let _ = self
-                                        .tx
-                                        .send(RobotMessage::DiscoveredObstacle { pos: target_pos });
-                                }
-                            }
-                            Tile::Resource(_) => {
-                                if self.known_resources.insert(target_pos) {
-                                    if let Some(res) = self.map.resources.get(&target_pos) {
-                                        let _ = self.tx.send(RobotMessage::DiscoveredResource {
-                                            pos: target_pos,
-                                            resource: res.clone(),
-                                        });
-                                    }
-                                }
-                            }
-                            _ => {}
+    fn check_tile(&mut self, target_pos: Pos) {
+        if let Some(tile) = self.map.get_tile(target_pos) {
+            match tile {
+                Tile::Obstacle => {
+                    if self.known_obstacles.insert(target_pos) {
+                        let _ = self
+                            .tx
+                            .send(RobotMessage::DiscoveredObstacle { pos: target_pos });
+                    }
+                }
+                Tile::Resource(_) => {
+                    if self.known_resources.insert(target_pos) {
+                        if let Some(res) = self.map.resources.get(&target_pos) {
+                            let _ = self.tx.send(RobotMessage::DiscoveredResource {
+                                pos: target_pos,
+                                resource: res.clone(),
+                            });
                         }
                     }
                 }
+                _ => {}
             }
         }
     }
 
-    fn choose_next_move(&self, rng: &mut impl rand::Rng) -> Option<Pos> {
+    fn valid_adjacent_positions(&self, pos: Pos) -> Vec<Pos> {
         let directions = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-        let mut valid_moves = Vec::new();
+        let mut valid = Vec::new();
 
         for (dx, dy) in directions {
-            let nx = self.pos.x as i32 + dx;
-            let ny = self.pos.y as i32 + dy;
+            let nx = pos.x as i32 + dx;
+            let ny = pos.y as i32 + dy;
 
             if nx >= 0 && ny >= 0 {
-                let next_pos = Pos {
+                let neighbor_pos = Pos {
                     x: nx as usize,
                     y: ny as usize,
                 };
-
-                if self.map.in_bounds(next_pos) && !self.known_obstacles.contains(&next_pos) {
-                    valid_moves.push(next_pos);
+                if self.map.in_bounds(neighbor_pos) {
+                    valid.push(neighbor_pos);
                 }
+            }
+        }
+        valid
+    }
+
+    fn choose_next_move(&self, rng: &mut impl rand::Rng) -> Option<Pos> {
+        let mut valid_moves = Vec::new();
+
+        for next_pos in self.valid_adjacent_positions(self.pos) {
+            if !self.known_obstacles.contains(&next_pos) {
+                valid_moves.push(next_pos);
             }
         }
 
@@ -118,6 +128,7 @@ mod tests {
     #[test]
     fn test_scout_only_discovers_resource_once() {
         let (tx, rx) = mpsc::channel();
+        let shutdown = Arc::new(AtomicBool::new(false));
 
         let mut resources = HashMap::new();
         let res_pos = Pos { x: 1, y: 0 };
@@ -137,7 +148,7 @@ mod tests {
             resources,
         });
 
-        let mut scout = Scout::new(1, tx, map);
+        let mut scout = Scout::new(1, tx, map, shutdown);
 
         scout.scan_surroundings();
         assert!(scout.known_resources.contains(&res_pos));
